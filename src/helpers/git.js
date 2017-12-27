e526ab453b7a088e0ba0a97f08824b17f29bb2f2
Jacob Marshall <git@jacobmarshall.co>
2017-12-28 11:25:47 +1300

    Make the git helper functions resolve `null` on error

--- src/helpers/git.js
+++ src/helpers/git.js
@@ -1,11 +1,18 @@
 import url from 'url';
 import child from 'child_process';
 
+const debug = require('debug')('webpack-bugsnag-plugin:git');
+
 function exec(cmd, options) {
   return new Promise((resolve, reject) => {
     child.exec(cmd, { cwd: options.path }, (err, stdout, stderr) => {
       if (err) {
-        reject(err);
+        if (options.volatile) {
+          reject(err);
+        } else {
+          debug(`exec "${cmd}" resulted in error: ${err.message}`);
+          resolve(null);
+        }
       } else {
         resolve(stdout.trim());
       }
@@ -22,7 +29,12 @@ function getLatestCommitHash(options) {
 }
 
 function getOriginRemoteUrl(options) {
-  return exec(`git config --get remote.origin.url`, options).then(getSafeRepositoryUrl);
+  return exec(`git config --get remote.origin.url`, {
+    ...options,
+    volatile: true,
+  })
+  .then(getSafeRepositoryUrl)
+  .catch(() => null);
 }
 
 function getSafeRepositoryUrl(repositoryUrl) {
